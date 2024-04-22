52cbb00ecf36b719d3518da0ba82d0fe6fe5ca72
Anjanique <anjaniquemackey@gmail.com>
2024-04-21 21:52:40 -0500

    add and configure react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+import Dashboard from './pages/Dashboard.jsx';
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<App />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+
+export default function Dashboard() {
+
+    return (
+        <h1>Welcome to the Dashboard</h1>
+    )
+}
