c84a825a5b89fee7896f24f00a49e5e804b17f8e
Anjanique <anjaniquemackey@gmail.com>
2024-04-26 14:57:10 -0500

    add signin page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,10 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 
-import './SignIn.css'
-
-
-
 export default function SignIn() {
 
-
-    
-    
-    const [user, setUser] = useState({ username: '', email: '', password: '' })
+    const [user, setUser] = useState({ username: '', email: '', password: '', timezone: ""  })
     const navigate = useNavigate();
-
-    console.log (user)
 
     async function registerUser(){
         const res = await fetch('http://127.0.0.1:5000/register',{
@@ -36,24 +27,66 @@ export default function SignIn() {
         console.log(user, 'form submitted');
         
         setUser({username:'',email:'',password:''});
-        
     }
 
     return (
         <>
+            <div className="
+            fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[2]
+            w-3/4 h-3/4 bg-white
+            custom-flex-row
+            ">
+                <img src="/signin-pic.png" alt="person working at desk" className="w-1/2 h-full object-cover" />
 
-<>
-            <h3>Create Account</h3>
-            <form action="" onSubmit={handleSubmit}>
-                <label htmlFor="username">New Username</label><br />
-                <input type="text" name='new username' value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}  required/><br />
-                <label htmlFor="email">Email</label><br />
-                <input type="email" name='email' value={user.email} onChange={(e)=> setUser({...user, email: e.target.value})} required/><br />
-                <label htmlFor="create password"> Create Password</label><br />
-                <input type="create password" name='create password' value={user.password} onChange={(e)=> setUser({...user, password: e.target.value})} required/><br />
-                <input type="Submit" value={'Register'}/>
-            </form>
-            </>
+                <div className="w-full custom-flex-col justify-center items-center gap-8">
+                    <div className="w-3/4">
+                        <h1 className="font-bold text-3xl">Sign In</h1>
+                        <span>Let&#39;s start this collboration</span>
+                    </div>
+
+                    <form onSubmit={handleSubmit} className="custom-flex-col justify-center items-center gap-5 w-full">
+
+                        <div className=" w-3/4">
+                            <label htmlFor="username" className="font-bold">Username</label>
+                            <input type="text" name='username' value={user.username}
+                                onChange={(e) => setUser((prevState) => ({ ...prevState, username: e.target.value }))}
+                                className="outline outline-primary-grey-500 rounded-custom-default outline-2 px-4 py-2 w-full"
+                                required
+                            />
+                        </div>
+
+                        <div className="w-3/4">
+                            <label htmlFor="email" className="font-bold">Email</label>
+                            <input type="text" name='email' value={user.email}
+                                onChange={(e) => setUser((prevState) => ({ ...prevState, email: e.target.value }))}
+                                className="outline outline-primary-grey-500 rounded-custom-default outline-2 px-4 py-2  w-full"
+                                required
+                            />
+                        </div>
+
+                        <div className="w-3/4">
+                            <label htmlFor="password" className="font-bold">Password</label>
+                            <input type="text" name='password' value={user.password}
+                                onChange={(e) => setUser((prevState) => ({ ...prevState, password: e.target.value }))}
+                                className="outline outline-primary-grey-500 rounded-custom-default outline-2 px-4 py-2  w-full"
+                                required
+                            />
+                        </div>
+
+                        <div className="w-3/4">
+                            <label htmlFor="timezone" className="font-bold">Time Zone</label>
+                            <input type="text" name='timezone' value={user.timezone}
+                                onChange={(e) => setUser((prevState) => ({ ...prevState, timezone: e.target.value }))}
+                                className="outline outline-primary-grey-500 rounded-custom-default outline-2 px-4 py-2  w-full"
+                                required
+                            />
+                        </div>
+
+                        <button className="bg-primary-green-500 text-white w-3/4 h-14 rounded-custom-default mt-9">Sign in</button>
+
+                    </form>
+                </div>
+            </div>
         </>
     )}
 
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import { CiSettings } from "react-icons/ci";
+import { PiBellLight } from "react-icons/pi";
+import { SlArrowDown } from "react-icons/sl";
+// import React from 'react';
 import SignIn from '../components/SignIn';
+// import Dashboard from '../components/Dashboard';
+import Sidebar from "../components/Sidebar";
+import MonthCalendar from "../components/MonthCalendar";
 
 import './SignInPage.css'
 
-export default function SignInPage({ children}) {
+export default function SignInPage() {
   return (
-    <div className='center'>
-       <SignIn/>
+    <>
+      {/* MODAL */}
+    <SignIn />
+      {/* OVERLAY */}
+     <div className="fixed z-[1] left-0 top-0 w-full h-full overflow-auto bg-black bg-opacity-[.60]"></div>
+    <div className="custom-flex-row ">
+     
+        {/* DAHSBOARD BACKGROUND */}
+            <Sidebar />
 
+            <div className="m-8 w-full">
 
-    </div>
+                {/* TOP OF PAGE AND NAVBAR/TOOLBAR */}
+                <div className="
+                custom-flex-row justify-between items-center
+                ">
+                    <span className="
+                    bg-secondary-blue-500 py-2.5 px-6 rounded-custom-full
+                    ">
+                        Project: Eternal Elegance Promotion
+                    </span>
+
+                    <nav className="custom-flex-row items-center gap-6">
+                        <CiSettings className="text-primary-grey-600 text-2xl"/>
+
+                        <PiBellLight className="text-primary-grey-600 text-2xl" />
+
+                        <div className="w-0.5 h-[35px] bg-primary-grey-400"></div>
+
+                        <div className="
+                        custom-flex-row justify-center items-center gap-4 
+                        border-[1px] border-primary-grey-500 rounded-custom-default py-2 px-4
+                        ">
+                            <div className="rounded-full bg-primary-grey-600 w-10 h-10"></div>
+                            <span>Luna</span>
+                            <SlArrowDown className="text-primary-grey-600 text-base" />
+                        </div>
+                    </nav>
+                </div>
+
+                <main className="mt-8">
+                    <MonthCalendar/>
+                </main>
+                </div>
+      </div>
+    </>
   )
 }
